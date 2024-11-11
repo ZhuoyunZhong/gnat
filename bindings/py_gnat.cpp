e3da831c3828a08e727a8092912841ab1923e76f
ZhuoyunZhong <zzhong3@wpi.edu>
2024-11-11 12:25:27 -0500

    rename fn names

--- bindings/py_gnat.cpp
+++ bindings/py_gnat.cpp
@@ -1,11 +1,11 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/functional.h>
-#include <pybind11/stl.h>             // For automatic STL container bindings
-#include <pybind11/numpy.h>           // For NumPy array support (if needed)
+#include <pybind11/stl.h>   // For automatic STL container bindings
+#include <pybind11/numpy.h> // For NumPy array support (if needed)
 #include "ompl/datastructures/NearestNeighborsGNAT.h"
 
 namespace py = pybind11;
-namespace ompl = ::ompl;              // Ensure the ompl namespace is accessible
+namespace ompl = ::ompl; // Ensure the ompl namespace is accessible
 
 // Define the type you will use with the GNAT.
 // For example, using a vector of doubles to represent a point in space.
@@ -18,27 +18,23 @@ PYBIND11_MODULE(_gnat, m)
 {
     py::class_<NearestNeighborsGNATPoint>(m, "NearestNeighborsGNAT")
         .def(py::init<>())
-        .def("setDistanceFunction", &NearestNeighborsGNATPoint::setDistanceFunction)
-        .def("add", (void (NearestNeighborsGNATPoint::*)(const Point&)) &NearestNeighborsGNATPoint::add)
-        .def("add_list", (void (NearestNeighborsGNATPoint::*)(const std::vector<Point>&)) &NearestNeighborsGNATPoint::add)
+        .def("set_distance_function", &NearestNeighborsGNATPoint::setDistanceFunction)
+        .def("add", (void(NearestNeighborsGNATPoint::*)(const Point &)) & NearestNeighborsGNATPoint::add)
+        .def("add_list", (void(NearestNeighborsGNATPoint::*)(const std::vector<Point> &)) & NearestNeighborsGNATPoint::add)
         .def("nearest", &NearestNeighborsGNATPoint::nearest)
-        .def("nearestK", [](NearestNeighborsGNATPoint &self, const Point &data, std::size_t k) {
+        .def("nearest_k", [](NearestNeighborsGNATPoint &self, const Point &data, std::size_t k) {
             std::vector<Point> nbh;
             self.nearestK(data, k, nbh);
-            return nbh;
-        })
-        .def("nearestR", [](NearestNeighborsGNATPoint &self, const Point &data, double radius) {
+            return nbh; })
+        .def("nearest_r", [](NearestNeighborsGNATPoint &self, const Point &data, double radius) {
             std::vector<Point> nbh;
             self.nearestR(data, radius, nbh);
-            return nbh;
-        })
+            return nbh; })
         .def("remove", &NearestNeighborsGNATPoint::remove)
         .def("clear", &NearestNeighborsGNATPoint::clear)
         .def("size", &NearestNeighborsGNATPoint::size)
         .def("list", [](NearestNeighborsGNATPoint &self) {
             std::vector<Point> data;
             self.list(data);
-            return data;
-        })
-        ;
+            return data; });
 }
